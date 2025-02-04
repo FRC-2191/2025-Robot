3be4890993193f7cb763b0b41d75cce699902309
Joshua Johnston <johnston.josh7@gmail.com>
2025-02-04 14:13:27 -0500

    Fix applyDeadzone and reduce strength of applyScalar

--- src/main/java/frc/robot/utils/JoystickUtils.java
+++ src/main/java/frc/robot/utils/JoystickUtils.java
@@ -1,16 +1,16 @@
 package frc.robot.utils;
 
 public abstract class JoystickUtils {
-    private static final double deadzone = 0.01;
+    private static final double deadzone = 0.1;
 
     public static double applyDeadzone (double input) {
-        if (Math.abs(input) < 0.1) {
+        if (Math.abs(input) < deadzone) {
             return 0;
         }
         return ((Math.abs(input) - deadzone)/(1 - deadzone)) * Math.signum(input);
     }
 
     public static double applyScaler (double input) {
-        return (.25 * input + .75 * Math.pow(input, 3));
+        return (0.7 * input + 0.4 * Math.pow(input, 3));
     }
 }
